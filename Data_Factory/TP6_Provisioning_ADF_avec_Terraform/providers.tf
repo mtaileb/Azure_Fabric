83bf735607eb207c358ef3af293ed854ec439709
mtaileb <116947757+mtaileb@users.noreply.github.com>
2024-03-29 13:24:20 +0100

    Create providers.tf

---
+++ Data_Factory/TP6_Provisioning_ADF_avec_Terraform/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.57.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
